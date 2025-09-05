af1f93e219605bdd9d0d54bbdcb7aa741299e96e
trudh <3373807551@qq.com>
2025-09-05 10:56:19 +0800

    初始化

---
+++ src/main/java/com/example/wangtu/WangTuApplication.java
@@ -0,0 +1,13 @@
+package com.example.wangtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangTuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangTuApplication.class, args);
+    }
+
+}
